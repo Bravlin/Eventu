fb215da2743915f83963a494ce50143026a09d9a
Bravlin <bravlinleonel@gmail.com>
2018-07-15 18:44:12 -0300

    Cambio a PHP.

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Eventu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/miestilo.css">
+    <link rel="stylesheet" type="text/css" href="css/bienvenida.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <header>
+            <div class="logo">
+                <img src="src/imagenes/logo.svg">Eventu
+            </div>
+        </header>
+        <div class="jumbotron text-center bienvenida">
+            <h1>Los momentos que importan, siempre con vos</h1>
+            <div><a class="btn eventuButton" href="">Registrarse</a></div>
+            <div><a href="login.html">¿Ya tienes una cuenta?</a></div>
+        </div>
+        <div class="container presentacion">
+            <h2 class="text-center">¿Qué es Eventu?</h2>
+            <div class="row">
+                <p class="col-sm-12 col-md-6">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+                <div class="col-sm-12 col-md-6">
+                    <img src="src/imagenes/tedx-sydney-events-in-sydney-andy-dexterity-472x233.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="container-fluid text-center">
+            <div class="logo">
+                <img src="src/imagenes/logo.svg">Eventu
+            </div>
+            <div class="redes">
+                <p>Síguenos a través de las redes</p>
+                <a href=""><i class="fa fa-facebook-official"></i></a>
+                <a href=""><i class="fa fa-twitter-square"></i></a>
+                <a href=""><i class="fa fa-instagram"></i></a>
+                <a href=""><i class="fa fa-google-plus-official"></i></a>
+            </div>
+            <p class="copyright"><i class="fa fa-copyright"></i>  Eventu 2018</p>
+        </div>  
+    </footer>
+</body>
+</html>
