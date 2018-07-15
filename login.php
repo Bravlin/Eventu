93bb7fd76cc50b56b403200bbe82094d2a973a03
Bravlin <bravlinleonel@gmail.com>
2018-07-15 19:07:34 -0300

    Incorporado footer-simple.

--- login.php
+++ login.php
@@ -34,20 +34,6 @@
             </form>
         </div>
     </div>
-    <footer>
-        <div class="container-fluid text-center">
-            <div class="logo">
-                <img src="src/imagenes/logo.svg">Eventu
-            </div>
-            <div class="redes">
-                <p>Síguenos a través de las redes</p>
-                <a href=""><i class="fa fa-facebook-official"></i></a>
-                <a href=""><i class="fa fa-twitter-square"></i></a>
-                <a href=""><i class="fa fa-instagram"></i></a>
-                <a href=""><i class="fa fa-google-plus-official"></i></a>
-            </div>
-            <p class="copyright"><i class="fa fa-copyright"></i>  Eventu 2018</p>
-        </div>  
-    </footer>
+    <?php require('footer-simple'); ?>
 </body>
 </html>
